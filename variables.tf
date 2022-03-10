8f85e6d38f5b5eb7ef4ec3e24aecb76bb72644e6
John-Sherer <John.Sherer@Smoothstack.com>
2022-03-10 04:45:24 -0500

    Added gitignore and removed sensitive files

--- variables.tf
+++ variables.tf
@@ -3,17 +3,52 @@ variable "vpc_cidr_block" {
     description = "The CIDR block for the VPC"
 }
 
-variable "private_subnet_cidr_block" {
+variable "private_cidr_block" {
     type = string
-    description = "The CIDR block for the private subnet"
+    description = "The CIDR block for the private resources"
 }
 
-variable "public_subnet_cidr_block" {
+variable "public_cidr_block" {
     type = string
-    description = "The CIDR block for the public subnet"
+    description = "The CIDR block for the public resources"
 }
 
 variable "aws-region" {
     type = string
     description = "The region to deploy our application"
+}
+
+variable "db-username" {
+    type = string
+    description = "username for database credentials"
+}
+
+variable "db-password" {
+    type = string
+    description = "password for database credentials"
+}
+
+variable "db-host" {
+    type = string
+    description = "Location of the database"
+}
+
+variable "db-port" {
+    type = string
+    description = "Port where the database can  be found"
+}
+
+variable "db-name" {
+    type = string
+    description = "Name of the specific database to reference"
+}
+
+variable "encrypt-secret-key" {
+    type = string
+    description = "Key for encryption in internal microservice communication"
+}
+
+variable "jwt-secret-key" {
+    type = string
+    description = "Encryption key for Java applications"
 }
