96594aa989f22db50cdf26366c37640995c2318c
John-Sherer <John.Sherer@Smoothstack.com>
2022-03-15 12:49:42 -0400

    created more nodegroups

--- main.tf
+++ main.tf
@@ -182,10 +182,28 @@ resource "aws_eks_node_group" "node_group" {
     node_role_arn = aws_iam_role.eks_cluster_iam_role.arn
     subnet_ids = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
 
+
     scaling_config {
         desired_size = 1
         max_size     = 1
         min_size     = 1
+    }
+}
+resource "aws_eks_node_group" "public_node_group" {
+    cluster_name = aws_eks_cluster.eks_cluster.name
+    node_group_name = "public_node_group"
+    node_role_arn = aws_iam_role.eks_cluster_iam_role.arn
+    subnet_ids = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+    
+
+    scaling_config {
+        desired_size = 1
+        max_size     = 1
+        min_size     = 1
+    }
+
+    tags = {
+        nodeGroup = "public_node_group"
     }
 }
 
